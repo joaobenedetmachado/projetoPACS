238240bb53d189d1b4c635e37a0cd082c1e8eb8f
João Benedet <105388117+joaobenedetmachado@users.noreply.github.com>
2024-05-17 19:50:57 -0300

    melhorei

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 $('.nav a[href^="#"]').on('click', function(e) {
@@ -9,4 +10,17 @@ $('.nav a[href^="#"]').on('click', function(e) {
       scrollTop: targetOffset - 100
     }, 500);
   });
+=======
+
+
+$('.nav a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 100
+    }, 500);
+  });
+>>>>>>> 5077dfac556ac1510b496f5ac284b31acfd7db71
   
